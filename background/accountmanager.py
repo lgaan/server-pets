76b624e7624cc4c4cc7e9f78ca9bfefbb4140ca5
Lgan <46571352+lganWebb@users.noreply.github.com>
2019-08-08 11:00:51 +0100

    Update accountmanager.py

--- background/accountmanager.py
+++ background/accountmanager.py
@@ -39,6 +39,11 @@ class AccountManager(commands.Cog):
             accounts = await self._bot.db.fetch("SELECT * FROM accounts")
 
             for account in accounts:
+                if self._bot.get_user(account["owner_id"] is None:
+                    await self._bot.db.execute("DELETE FROM accounts WHERE owner_id = $1", account["owner_id"]
+                    
+                    continue
+                                               
                 if account["pets"]: 
                     pets = json.loads(account["pets"])      
 
