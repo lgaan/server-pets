562553c3c55e6f5ba99018e4b0d844083bfdeb81
Logan Webb <lganwebb04@gmail.com>
2019-08-27 18:24:15 +0100

    Fixed settings bug

--- cogs/setting.py
+++ cogs/setting.py
@@ -5,9 +5,12 @@ import discord
 from discord.ext import commands
 
 from utils.paginator import SettingsPaginator
+from utils.managers.accountmanager import AccountManager
+
 class Settings(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        self.manager = AccountManager(bot)
 
         self.conversions = {
             True: "<:greenTick:596576670815879169>",
@@ -18,27 +21,30 @@ class Settings(commands.Cog):
     @commands.command(name="settings")
     async def settings_(self, ctx):
         """A command for showing the bot settings for your account, or changing them."""
-        account = await self.bot.db.fetch("SELECT * FROM accounts WHERE owner_id = $1", ctx.author.id)
+        try:
+            account = await self.manager.get_account(ctx.author.id)
 
-        if not account:
-            return await ctx.send("You do not have an account. To create one use `p-create`")
+            if not account:
+                return await ctx.send("You do not have an account. To create one use `p-create`")
 
-        account_settings = json.loads(account[0]["settings"])
+            account_settings = json.loads(account.settings)
 
-        embed = discord.Embed(title=f"{ctx.author.name}'s settings", description="Use the arrows to navigate between settings, use the switch emoji to change the setting.", colour=discord.Colour.blue(), timestamp=ctx.message.created_at)
+            embed = discord.Embed(title=f"{ctx.author.name}'s settings", description="Use the arrows to navigate between settings, use the switch emoji to change the setting.", colour=discord.Colour.blue(), timestamp=ctx.message.created_at)
 
-        incr = 0
-        for key, value in account_settings.items():
-            val = f"{key[0].upper()}{key[1:]}".replace("_"," ")
-            if incr == 0:
-                embed.add_field(name=f"> {val}", value=self.conversions[value])
-            else:
-                embed.add_field(name=val, value=self.conversions[value])
-            
-            incr += 1
+            incr = 0
+            for key, value in account_settings.items():
+                val = f"{key[0].upper()}{key[1:]}".replace("_"," ")
+                if incr == 0:
+                    embed.add_field(name=f"> {val}", value=self.conversions[value])
+                else:
+                    embed.add_field(name=val, value=self.conversions[value])
+                
+                incr += 1
 
-        msg = await ctx.send(embed=embed)
-        return await SettingsPaginator(ctx=ctx, message=msg, entries=[key for key in account_settings.keys()]).paginate()
+            msg = await ctx.send(embed=embed)
+            return await SettingsPaginator(ctx=ctx, message=msg, entries=[key for key in account_settings.keys()]).paginate()
+        except Exception:
+            traceback.print_exc()
 
 def setup(bot):
     bot.add_cog(Settings(bot))
