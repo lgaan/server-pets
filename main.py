c674094c2080158a2be6ac26a7e7de33dd35814f
Lgan <46571352+lganWebb@users.noreply.github.com>
2020-02-05 07:36:33 +0000

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import json
 
 import aiohttp
 
-import dblpy
+import dbl
 
 from discord.ext import commands
 from pathlib import Path
@@ -36,7 +36,7 @@ class Bot(commands.AutoShardedBot):
         self.remove_command("help")
         self.load_extension("jishaku")
 
-        self.dbl = dblpy.DBLClient(self, os.environ.get("DBL_TOKEN"), webhook_path="/dblwebhook", webhook_auth="Axc541", webhook_port=5000)
+        self.dbl = dbl.DBLClient(self, os.environ.get("DBL_TOKEN"), webhook_path="/dblwebhook", webhook_auth="Axc541", webhook_port=5000)
         self.db = None
 
         self.usage = {}
