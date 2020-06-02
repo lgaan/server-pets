bcb6bd5c02e81e4f6f88681faab99f5c15a2cb9a
Logan Webb <lganwebb04@gmail.com>
2020-06-02 18:24:54 +0100

    this bot is now running on old code, prefix has changed to `p!`

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import traceback
 import os
 import asyncpg
 import discord
-import json
+import json 
 
 import aiohttp
 
@@ -32,11 +32,10 @@ class BotContext(commands.Context):
 
 class Bot(commands.AutoShardedBot):
     def __init__(self):
-        super().__init__(command_prefix="p-", case_insensitive=True)
+        super().__init__(command_prefix="p!", case_insensitive=True)
         self.remove_command("help")
         self.load_extension("jishaku")
 
-        self.dbl = dbl.DBLClient(self, os.environ.get("DBL_TOKEN"), webhook_path="/dblwebhook", webhook_auth="Axc541", webhook_port=5000)
         self.db = None
 
         self.usage = {}
