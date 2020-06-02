ed224e0d7abcc829834a25d9fc962e6e618ba779
Logan Webb <lganwebb04@gmail.com>
2020-06-02 18:28:34 +0100

    fix

--- background/dbl.py
+++ background/dbl.py
@@ -9,7 +9,7 @@ from discord.ext import commands, tasks
 class DiscordBotList(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.dblpy = bot.dbl
+        #self.dblpy = bot.dbl
 
         #self.update_stats.start()
 
